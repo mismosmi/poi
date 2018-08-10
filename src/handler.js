1836c8d483f586eba983eb9611f1635f20b1c22a
Michel Smola <m@michelsmola.de>
2018-08-10 10:36:57 +0200

    comments

--- src/handler.js
+++ src/handler.js
@@ -3,10 +3,18 @@ const data = require('../data/berlin.json');
 // export handler function
 module.exports = async ctx => {
   ctx.assert(ctx.method, 'GET', 'This API currently only supports GET-Requests');
+
+  // query.by defines what to search for
   switch (ctx.query.by) {
     case 'id':
+      // minimal example: here we just get one entry by id
       return ctx.body = data.find( row => row.id = ctx.query.id );
     case 'boundingbox':
+      // here we look for entries within the bounding box defined by
+      // minimum latitude: latMin
+      // maximum latitude: latMax
+      // minimum longitude: lngMin
+      // maximum longitude: lngMax
       return ctx.body = data.filter( row => {
         return row.lat > ctx.query.latMin &&
           row.lat < ctx.query.latMax &&
